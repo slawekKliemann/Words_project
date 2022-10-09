7045338c33d1c5cdc47acd1fa18d8c255a891dcc
slawekKliemann <slawek.kliemann@gmail.com>
2022-10-09 11:40:37 +0200

    add footer section

---
+++ src/components/common/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export function Footer(){
+
+    return(
+        <>
+        <footer>
+            <div className="footer_copyright">
+                <p>Copyright &#169; 2022 by Sławek Kliemann</p>
+            </div>
+            <div>
+                <div className="footer_social">
+                    <a className="footer_social-link" href="https://github.com/slawekKliemann"><i className="fa-brands fa-github"></i></a>
+                    <a className="footer_social-link" href="https://www.facebook.com/profile.php?id=100001944232400"><i className="fa-brands fa-facebook"></i></a>
+                </div>
+            </div>
+        </footer>
+        </>
+    )
+}
