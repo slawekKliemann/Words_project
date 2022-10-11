585aa56d0695fe6f745f89b1c767a1899e1baf15
slawekKliemann <slawek.kliemann@gmail.com>
2022-10-11 19:26:26 +0200

    added RandomQ function in MainGame.js

--- src/components/pages/MainGame.js
+++ src/components/pages/MainGame.js
@@ -53,35 +53,50 @@ export function MainGame(){
     )
 }
 */
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 /*import goodAnswer from '../../images/goodAnswer.png'
 import badAnswer from '../../images/badAnswer.png'*/
 import letsPlay from '../../images/letsPlay.png'
 const API = "http://localhost:3000";
 
 
-fetch(`${API}/words`)
-    .then(response => response.json())
-    .then(data =>{
-        // console.log(data)
-        const quest = data.map((item)=>{
-            return item
-        })
+function RandomQ() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [task, setTask] = useState([])
 
-        const randomQuest = quest[Math.floor(Math.random()*quest.length)]
-        console.log(randomQuest)
-        console.log(randomQuest.description)
-        console.log(randomQuest.answer)
-        console.log(randomQuest.answer.length)
-        console.log(randomQuest.id)
-
+    const quest = task.map((item)=>{
+        return item
     })
+    const randomQuest = quest[Math.floor(Math.random()*quest.length)]
 
-    .catch(error => {
-        console.log(error);
-    });
-
-
+    useEffect(() => {
+        fetch(`${API}/words`)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setTask(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <>
+                <h2>{randomQuest.description}</h2>
+                <h3>Ilość liter w wyrazie: {randomQuest.answer.length}</h3>
+            </>
+        );
+    }
+}
 
 export function MainGame(){
     const [words, setWords] = useState('')
@@ -97,8 +112,9 @@ export function MainGame(){
                 <img className="mainGame_character-img" src={letsPlay} alt="Postać detektywa witająca w aplikacji"/>
             </div>
             <div className="mainGame_description">
-                <h2 className="mainGame_description-header">TUTAJ BĘDZIE PYTANIE</h2>
-                <h3 className="mainGame_description-header">TUTAJ BĘDZIE ILOŚĆ LITER W WYRAZIE</h3>
+                <RandomQ/>
+                {/*<h2 className="mainGame_description-header">TUTAJ BĘDZIE PYTANIE</h2>*/}
+                {/*<h3 className="mainGame_description-header">TUTAJ BĘDZIE ILOŚĆ LITER W WYRAZIE</h3>*/}
                 <input type="text" value={words} onChange={e => setWords(e.target.value)} placeholder="WPISZ ODPOWIEDŹ"/>
                 <button>LOSUJ</button>
                 <button onClick={handleCheck}>SPRAWDŹ</button>
