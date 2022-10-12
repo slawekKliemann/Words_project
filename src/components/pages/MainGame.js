fe77ce14695f50d3415175faa49c21affadf9a46
slawekKliemann <slawek.kliemann@gmail.com>
2022-10-12 20:20:18 +0200

    added random function in MainGame.js

--- src/components/pages/MainGame.js
+++ src/components/pages/MainGame.js
@@ -1,94 +1,3 @@
-/*
-import React, {useEffect, useState} from "react";
-/!*import goodAnswer from '../../images/goodAnswer.png'
-import badAnswer from '../../images/badAnswer.png'*!/
-import letsPlay from '../../images/letsPlay.png'
-const API = "http://localhost:3000";
-
-
-function RandomQ() {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [task, setTask] = useState([])
-
-    const quest = task.map((item)=>{
-        return item
-    })
-    const randomQuest = quest[Math.floor(Math.random()*quest.length)]
-
-    useEffect(() => {
-        fetch(`${API}/words`)
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    setIsLoaded(true);
-                    setTask(result);
-                },
-                (error) => {
-                    setIsLoaded(true);
-                    setError(error);
-                }
-            )
-    }, [])
-    if (error) {
-        return <div>O NIE! Zapodziałem gdzieś swoje notatki. Wróć za chwilę! </div>;
-    } else if (!isLoaded) {
-        return <div>Szukam zadania dla Ciebie...</div>;
-    } else {
-        return (
-            <>
-                <h2>{randomQuest.description}</h2>
-                <h3>Liczba liter w wyrazie: {randomQuest.answer.length}</h3>
-                <Form prop={randomQuest.answer}/>
-            </>
-        );
-    }
-}
-
-function Form(props){
-    const [words, setWords] = useState('')
-
-    function check(event){
-        event.preventDefault()
-
-    }
-
-    if (props.prop.toLowerCase()===words.toLowerCase()){
-        return(
-            <div>GRATULACJE! <strong>{words}</strong> to poprawna odpowiedź</div>
-        )
-    }
-
-    return(
-        <form>
-            <label>
-                Tutaj wpisz odpowiedź
-            <input type="text" value={words} onChange={e => setWords(e.target.value)} placeholder="WPISZ ODPOWIEDŹ"/>
-            </label>
-            <input type="submit" value="SPRAWDŹ" onClick={check}/>
-        </form>
-    )
-
-}
-
-export function MainGame(){
-
-    function random(){
-
-    }
-    return(
-        <main className="mainGame">
-            <div className="mainGame_character">
-                <img className="mainGame_character-img" src={letsPlay} alt="Postać detektywa witająca w aplikacji"/>
-            </div>
-            <div className="mainGame_description">
-                <RandomQ/>
-                <button onClick={random}>LOSUJ</button>
-            </div>
-        </main>
-    )
-}
-*/
 import React, {useEffect, useState} from "react";
 /*import goodAnswer from '../../images/goodAnswer.png'
 import badAnswer from '../../images/badAnswer.png'*/
@@ -101,10 +10,6 @@ function RandomQ() {
     const [isLoaded, setIsLoaded] = useState(false);
     const [task, setTask] = useState([])
 
-    function random(){
-
-    }
-
 
     const quest = task.map((item)=>{
         return item
@@ -135,7 +40,6 @@ function RandomQ() {
                 <h2>{randomQuest.description}</h2>
                 <h3>Liczba liter w wyrazie: {randomQuest.answer.length}</h3>
                 <Form prop={randomQuest.answer}/>
-                <button onClick={random}>LOSUJ</button>
             </>
         );
     }
@@ -144,6 +48,10 @@ function RandomQ() {
 function Form(props){
     const [words, setWords] = useState('')
     const [reply, setReply] = useState(null)
+
+    function random(){
+        window.location.reload();
+    }
 
     function check(event){
         event.preventDefault()
@@ -156,11 +64,17 @@ function Form(props){
 
    if (reply){
        return(
-           <div>GRATULACJE! <strong>{words.toUpperCase()}</strong> to poprawna odpowiedź</div>
+           <>
+             <div>GRATULACJE! <strong>{words.toUpperCase()}</strong> to poprawna odpowiedź</div>
+             <button onClick={random}>Losuj</button>
+           </>
        )
    }if (reply === false){
        return(
-           <div>Niestety nie. Prawidłowa odpowiedź to <strong>{props.prop.toUpperCase()}</strong> Ale nie martw się, to zadanie było trudne</div>
+           <>
+             <div>Niestety nie. Prawidłowa odpowiedź to <strong>{props.prop.toUpperCase()}</strong> ale nie martw się, to zadanie było trudne</div>
+             <button onClick={random}>Losuj</button>
+           </>
        )
     }
 
@@ -178,9 +92,6 @@ function Form(props){
 
 export function MainGame(){
 
-    // function random(){
-    //
-    // }
     return(
         <main className="mainGame">
             <div className="mainGame_character">
@@ -188,7 +99,6 @@ export function MainGame(){
             </div>
             <div className="mainGame_description">
                 <RandomQ/>
-               {/* <button onClick={random}>LOSUJ</button>*/}
             </div>
         </main>
     )
